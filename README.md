19c6e27c7e9a368d69f18469ffb0e2b4821ddac8
tomnijhof <tom.nijhof@cytosmart.com>
2022-07-28 08:49:05 +0200

    init
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+## Local testing
+
+Make a env.list with NEO4J_URL, NEO4J_USER, and NEO4J_PSWD.
+Run 
+
+```cmd
+uvicorn app.main:app --env-file env.list --port 81
+```
---
+++ app/main.py
@@ -0,0 +1,70 @@
+import os
+import regex as re
+from fastapi import FastAPI
+from py2neo import Graph
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+url = os.environ["NEO4J_URL"]
+user = os.environ["NEO4J_USER"]
+pswd = os.environ["NEO4J_PSWD"]
+
+graph = Graph(url, auth=(user, pswd))
+
+"""
+Synonyms with multiple compounds
+
+"pubChemSynId": "69da37703943cfa2a7a50159cfb1fc95",
+"name": "( inverted exclamation marka)-potassium citramalate monohydrate"
+
+"pubChemSynId": "0f2e32e0f1a7f2cc034303e4ff2b7948",
+"name": "((1-aza-2-(3-thienyl)vinyl)amino)(prop-2-enylamino)methane-1-thione"
+
+"pubChemSynId": "23000d280089903b877dbe0f2b4ff646",
+"name": "(+)-(18-crown-6)-2,3,11,12-tetracarboxamide"
+
+"pubChemSynId": "b20b0d4197f8609dc17d1af12e1ca715",
+"name": "(+)-2,3,4,4a,5,9b-hexahydro-5-(4-aminophenyl)-1h-indeno(1,2-b)pyridine"
+
+"pubChemSynId": "1b464664b35b4cef85295082b46703b1",
+"name": "(+)-2-methylamino-2-phenylpropane hydrochloride"
+
+"""
+
+
+@app.post("/symAutoComplete/")
+async def read_item(chemical_name: str):
+
+    # Find alls unicode characters (\p) classified as letters ({L}) 
+    # in a group of at least 2 ({2,})
+    # This also helps against (some) forms of cypher injects
+    all_words = re.findall(r"\p{L}{2,}", chemical_name)
+    
+    all_fuzzy_names = "~ AND ".join(all_words) +"~"
+    response = graph.run(
+        f""" 
+        CALL {{
+            CALL db.index.fulltext.queryNodes('synonymsFullText', "{all_fuzzy_names}")
+            YIELD node, score
+            return node, score limit 50
+        }}
+        MATCH (node)-[:IS_ATTRIBUTE_OF]->(c:Compound)
+        WITH DISTINCT c as c, collect({{score: score, node: node}})[0] as s
+        WITH DISTINCT s as s, collect(c.pubChemCompId) as compoundId
+        RETURN s.node.name as name, s.node.pubChemSynId as synonymId, compoundId limit 5
+        """
+    ).data()
+
+    return response
